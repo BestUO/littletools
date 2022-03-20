6fea76e8be987bd03fe774b301deb5303d7d47fc
Boray <250316792@qq.com>
2022-03-20 14:44:39 +0000

    修改线程池为可伸缩，同时提供task非new的方法

--- tools/threadpool.hpp
+++ tools/threadpool.hpp
@@ -10,15 +10,18 @@
 #include <functional>
 #include <stdexcept>
 #include "../queue/threadsafecontainer.hpp"
+#include <atomic>
 
 class ThreadPool {
 public:
-    ThreadPool(size_t);
+    ThreadPool(size_t minsize, size_t maxsize=32);
     template<class F, class... Args>
     auto enqueue(F&& f, Args&&... args) 
         -> std::future<typename std::result_of<F(Args...)>::type>;
     ~ThreadPool();
 private:
+    size_t minsize;
+    size_t maxsize;
     // need to keep track of threads so we can join them
     std::vector< std::thread > workers;
     // the task queue
@@ -26,38 +29,46 @@ private:
     LockQueue<std::function<void()>,10240> __queuetask;
     
     // synchronization
-    // std::mutex queue_mutex;
-    // std::condition_variable condition;
+    // std::mutex mtx;
+    // std::condition_variable cv;
+    std::atomic<unsigned int> totalnum;
+    std::thread CreateWorker(bool original);
 };
- 
-// the constructor just launches some amount of workers
-inline ThreadPool::ThreadPool(size_t threads)
+
+inline std::thread ThreadPool::CreateWorker(bool original)
 {
-    for(size_t i = 0;i<threads;++i)
-        workers.emplace_back(
-            [this]
+    std::thread t( [this,original]
+    {
+        totalnum++;
+        for(;;)
+        {
+            auto [flag, e] = __queuetask.GetObjBulk();
+            if(flag)
             {
-                for(;;)
+                while(!e.empty())
                 {
-                    // {
-                    //     std::unique_lock<std::mutex> lock(this->queue_mutex);
-                    //     this->condition.wait(lock,[this]{ return !__queuetask.IsEmpty(); });
-                    // }
-                    
-                    auto [flag, e] = __queuetask.GetObjBulk();
-                    if(flag)
-                    {
-                        while(!e.empty())
-                        {
-                            e.front()();
-                            e.pop();
-                        }
-                    }
-                    else
-                        std::this_thread::sleep_for(std::chrono::seconds(1));
+                    e.front()();
+                    e.pop();
                 }
             }
-        );
+            else
+            {
+                if(!original)
+                    break;
+                std::this_thread::sleep_for(std::chrono::seconds(1));
+            }
+        }
+        totalnum--;
+    });
+    
+    return t;
+}
+ 
+// the constructor just launches some amount of workers
+inline ThreadPool::ThreadPool(size_t minsize, size_t maxsize):minsize(minsize),maxsize(maxsize)
+{
+    for(size_t i = 0;i<minsize;++i)
+        workers.emplace_back(std::move(CreateWorker(true)));
 }
 
 // add new work item to the pool
@@ -67,12 +78,22 @@ auto ThreadPool::enqueue(F&& f, Args&&... args)
 {
     using return_type = typename std::result_of<F(Args...)>::type;
 
+    // with -std=c++2b you can also code as:
+    // auto task = std::packaged_task<return_type()>(std::bind(std::forward<F>(f), std::forward<Args>(args)...));
+    // while(!__queuetask.AddObj([task=std::move(task)]()mutable{ (*task)(); }))
+    // {...}
+    
     auto task = std::make_shared< std::packaged_task<return_type()> >(
             std::bind(std::forward<F>(f), std::forward<Args>(args)...)
         );
-        
+    
     std::future<return_type> res = task->get_future();
-    __queuetask.AddObj([task](){ (*task)(); });
+    while(!__queuetask.AddObj([task](){ (*task)(); }))
+    {
+        if(totalnum < maxsize)
+            CreateWorker(false).detach();
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
     // condition.notify_one();
     return res;
 }
