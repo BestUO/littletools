cbe4ab1e5917a21465f1db852e8c2e9b77b407e9
Parsifal <807128216@qq.com>
2022-05-09 05:54:39 +0000

    jsoncpp重写解析消息

--- toolcompose.cpp
+++ toolcompose.cpp
@@ -4,6 +4,7 @@
 #include "ormpp/dbng.hpp"
 #include "ormpp/mysql.hpp"
 #include "settingParser/settingParser.h"
+#include "work/newstructure/GetCallRecord.h"
 struct aicall_tts_file_cache
 {
 	int id;
@@ -53,9 +54,15 @@ protected:
     // typename std::enable_if<std::is_same<typename GetContainerType<T>::Type, Worker2Params>::value>::type
     DealElement(ormpp::dbng<ormpp::mysql> &mysql, std::string &&s)
     {
-        auto res = mysql.query<aicall_tts_file_cache>("id = 5622");
-        for(auto& file : res)
-            std::cout<<file.id<<" "<<file.TTS_text<<" "<<file.TTS_version_code<<std::endl;
+      s="{\"status\":0,\"info\":\"请求成功\",\"data\":{\"enterprise_type\":1,\"record_url\":\"https:\\/\\/ks3-cn-beijing.ksyun.com\\/yimi-record-1-year\\/1000_00020a4f_918705532687_10001_10000000001_2_20220407_145607_145615_8_1344299.mp3?Expires=1649321794&response-content-type=audio%2Fmp3&KSSAccessKeyId=AKLT0ChC2dqZRK6viMM8BwGZLQ&Signature=7qezhD2bmtkGFWWxueSflOI2S%2BE%3D\",\"records\":[{\"call_type\":\"1\",\"call_state\":\"15\",\"cc_number\":\"1649314541655804conf_1649314555346\",\"dialing\":\"1000_00020a4f\",\"incoming\":\"918705532687\",\"confirm_timestamp\":\"1649314567\",\"start_time\":\"1649314555\",\"end_time\":\"1649314575\",\"duration_time\":\"8\",\"record_filename\":\"1000_00020a4f_918705532687_10001_10000000001_2_20220407_145607_145615_8.mp3\",\"record_status\":\"3\",\"res_token\":null,\"enqueue_timestamp\":\"0\",\"send_query_msg_timestamp\":\"0\",\"recv_answer_msg_timestamp\":\"0\",\"send_invite_timestamp\":\"1649314558\",\"static_time_index\":\"1649314800\",\"switch_number\":\"02566203172\",\"asr_state\":\"\",\"final_ring_time\":9,\"call_result\":1}]}}" ;
+
+        CallRecord record;
+        CallInfo ca = record.GetCallRecord(s,2);
+        cout<<ca.call_state<<endl;
+
+        // auto res = mysql.query<aicall_tts_file_cache>("id = 5622");
+        // for(auto& file : res)
+        //     std::cout<<file.id<<" "<<file.TTS_text<<" "<<file.TTS_version_code<<std::endl;
     }
 
     virtual typename std::enable_if<std::is_same<typename T::Type, std::string>::value>::type
@@ -89,7 +96,12 @@ int main()
     // SetApiCallBackHandler(server, threadpool);
 
 	// server.run();
-    settingParser pa;
-    cout<<pa.GetMysqlPassord();
+    // settingParser pa;
+    // cout<<pa.GetMysqlPassord();
+   std::string s="{\"status\":0,\"info\":\"请求成功\",\"data\":{\"enterprise_type\":1,\"record_url\":\"https:\\/\\/ks3-cn-beijing.ksyun.com\\/yimi-record-1-year\\/1000_00020a4f_918705532687_10001_10000000001_2_20220407_145607_145615_8_1344299.mp3?Expires=1649321794&response-content-type=audio%2Fmp3&KSSAccessKeyId=AKLT0ChC2dqZRK6viMM8BwGZLQ&Signature=7qezhD2bmtkGFWWxueSflOI2S%2BE%3D\",\"records\":[{\"call_type\":\"1\",\"call_state\":\"15\",\"cc_number\":\"1649314541655804conf_1649314555346\",\"dialing\":\"1000_00020a4f\",\"incoming\":\"918705532687\",\"confirm_timestamp\":\"1649314567\",\"start_time\":\"1649314555\",\"end_time\":\"1649314575\",\"duration_time\":\"8秒\",\"record_filename\":\"1000_00020a4f_918705532687_10001_10000000001_2_20220407_145607_145615_8.mp3\",\"record_status\":\"3\",\"res_token\":null,\"enqueue_timestamp\":\"0\",\"send_query_msg_timestamp\":\"0\",\"recv_answer_msg_timestamp\":\"0\",\"send_invite_timestamp\":\"1649314558\",\"static_time_index\":\"1649314800\",\"switch_number\":\"02566203172\",\"asr_state\":\"\",\"final_ring_time\":9,\"call_result\":1}]}}" ;
+
+        CallRecord record;
+        CallInfo ca = record.GetCallRecord(s,2);
+        cout<<ca.duration_time<<endl;
 	return 0;
 }
