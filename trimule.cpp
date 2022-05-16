e7e465a08f038d67735e6796837cf22f90802a11
Parsifal <807128216@qq.com>
2022-05-16 03:29:35 +0000

    info

--- trimule.cpp
+++ trimule.cpp
@@ -79,6 +79,7 @@ void SetApiCallBackHandler(cinatra::http_server &server, T threadpool)
 {
     server.set_http_handler<cinatra::GET, cinatra::POST>("/", [threadpool = threadpool](cinatra::request &req, cinatra::response &res)
     {
+        LOGGER->info("news is ",req.body());
         CallRecord check;
         std::string check_res = check.CheckInfo(std::string(req.body()));
         if(check_res!="900"&&check_res!="901")
