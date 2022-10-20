6176c1f3d4c0684e18d478605251a36dbc9b82db
paxifaer <807128216@qq.com>
2022-10-20 20:12:00 +0800

    1

--- dbstruct/newstructure/CallBack/CallBack.cpp
+++ dbstruct/newstructure/CallBack/CallBack.cpp
@@ -330,6 +330,8 @@ CallBackRules CallBackManage::MakeCallBackRulesFromMySql(const std::tuple<std::s
         rules.auto_recall_status = stoi_s(std::get<static_cast<int>(outcall_task_enum::auto_recall_status)>(result_outcall_task[0]));
         rules.auto_recall_max_times = stoi_s(std::get<static_cast<int>(outcall_task_enum::auto_recall_max_times)>(result_outcall_task[0]));
         rules.delete_flag = std::get<4>(result_outcall_task[0]);
+        cout<<"rules.delete_flag"<<rules.delete_flag;
+        LOGGER->info("rules.delete_flag   ", rules.delete_flag);
     }
     else
     {
