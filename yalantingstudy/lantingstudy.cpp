986d6c468b13651effac808beb38d4b22cf97d54
BestUO <250316792@qq.com>
2023-03-28 02:35:59 +0000

    update asio and yalanting

--- yalantingstudy/lantingstudy.cpp
+++ yalantingstudy/lantingstudy.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include "struct_pack/struct_pack.hpp"
+#include "coro_rpc/coro_rpc_server.hpp"
+#include "coro_rpc/coro_rpc_client.hpp"
+#include "coro_http/coro_http_client.h"
 
-void t()
+void struct_pack_test()
 {
     struct person 
     {
@@ -25,8 +28,47 @@ void t()
     auto ec = struct_pack::deserialize_to(person3, result);
 }
 
+inline std::string_view echo(std::string_view str) 
+{ 
+    return str; 
+}
+
+void core_rpc_server_test()
+{
+    coro_rpc::coro_rpc_server server(/*thread_num =*/10, /*port =*/9000);
+    server.register_handler<echo>(); // register function echo
+    auto err = server.start(); // start the server & block
+}
+
+async_simple::coro::Lazy<void> core_rpc_client() 
+{
+    coro_rpc::coro_rpc_client client;
+    co_await client.connect("localhost", /*port =*/"9000"); // connect to the server
+
+    auto r = co_await client.call<echo>("hello coro_rpc"); // call remote function echo
+    std::cout << r.value() << "\n"; //will print "hello coro_rpc"
+}
+
+async_simple::coro::Lazy<void> get_post(cinatra::coro_http_client &client) {
+  std::string uri = "http://www.example.com";
+  auto result = co_await client.async_get(uri);
+  std::cout << result.status << "\n";
+  
+  result = co_await client.async_post(uri, "hello", ylt::req_content_type::string);
+  std::cout << result.status << "\n";
+}
+
+void core_rpc_asynic_client_test()
+{
+    cinatra::coro_http_client client{};
+    async_simple::coro::syncAwait(get_post(client));
+}
+
 int main()
 {
-    t();
+    // struct_pack_test();
+    core_rpc_server_test();
+    // syncAwait(core_rpc_client());
+    // core_rpc_asynic_client_test();
     return 0;  
 }
